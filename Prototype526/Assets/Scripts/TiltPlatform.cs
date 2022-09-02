471869db71de4f4c7a17524c1867903ec4027289
Lizzet Clifton <lizzetclifton@Lizzets-MBP-306.lan>
2022-09-01 22:25:11 -0700

    platform not rotating anymore, platform rotation depends on distance from center

--- Prototype526/Assets/Scripts/TiltPlatform.cs
+++ Prototype526/Assets/Scripts/TiltPlatform.cs
@@ -19,16 +19,26 @@ public class TiltPlatform : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
-        Debug.Log("player position: " + playerPosition);
-        Debug.Log("center of platform: " + platformCenter);
+        // Debug.Log("player position: " + playerPosition);
+        // Debug.Log("center of platform: " + platformCenter);
 
         centerToPlayer = playerPosition - platformCenter; //creating vector from the center of platform to the player position
-        Debug.DrawLine(platformCenter, playerPosition, Color.white, 2.5f); //drawing the vector for debugging purposes
+        // Debug.DrawLine(platformCenter, playerPosition, Color.white, 2.5f); //drawing the vector for debugging purposes
 
         //axis of rotation will be in the direction of the centerToPlayer direction
-        if (playerPosition != platformCenter) { //was trying to stop the platform moving when we havent moved the player
-            transform.Rotate(centerToPlayer, 0.075f); //this is rotating around an axis that is the direction of the vector from the center of the platform to our player. am just using 0.075 degrees because it seemed to be a good rate of rotation
-        }
+        transform.Rotate(centerToPlayer, centerToPlayer.magnitude * 0.035f); //this is rotating around an axis that is the direction of the vector from the center of the platform to our player. am just using 0.035 degrees because it seemed to be a good rate of rotation. am multiplying by the vector's magnitude so that it tilts more when we get closer to the edges
         
+    }
+
+    void OnCollisionExit(Collision collisionInfo) {
+        // if (collisionInfo.gameObject.tag == "Player") {
+            print("No longer in contact with " + collisionInfo.transform.name);
+        // }
+    }
+
+	void OnCollisionEnter(Collision collisionInfo) {
+        // if (collisionInfo.gameObject.tag == "Player") {
+            print("Again in contact with " + collisionInfo.transform.name);
+        // }
     }
 }
