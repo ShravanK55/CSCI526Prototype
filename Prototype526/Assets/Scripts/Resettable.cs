87683424c0b85a6c2ef0154ab125f8e6bb1fde7f
Shravan Kumar <shravan.k55@protonmail.com>
2022-09-05 17:50:37 -0700

    Added code to reset objects on player death.

---
+++ Prototype526/Assets/Scripts/Resettable.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resettable : MonoBehaviour
+{
+    private Vector3 _originalPosition;
+    private Quaternion _originalRotation;
+    
+    void Start()
+    {
+        _originalPosition = transform.position;
+        _originalRotation = transform.rotation;
+    }
+
+    public void ResetTransform()
+    {
+        transform.position = _originalPosition;
+        transform.rotation = _originalRotation;
+    }
+}
--- Prototype526/Assets/Scripts/TiltPlatform.cs
+++ Prototype526/Assets/Scripts/TiltPlatform.cs
@@ -1,6 +1,8 @@
 using System;
 using UnityEngine;
 using System.Collections;
+using System.Numerics;
+using Vector3 = UnityEngine.Vector3;
 
 public class TiltPlatform : MonoBehaviour {
 
